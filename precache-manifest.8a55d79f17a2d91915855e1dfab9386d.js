e179bf8fee2652a3a4a34e36bd810371aa331c3d
shivkaansh <shivansh.sharma706@gmail.com>
2020-10-28 22:28:20 +0530

    Updates

---
+++ precache-manifest.8a55d79f17a2d91915855e1dfab9386d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52bf82d10ed1db963e0181833981451",
+    "url": "/Amazon_clone_reactJS/index.html"
+  },
+  {
+    "revision": "0c91811896b74c9489a2",
+    "url": "/Amazon_clone_reactJS/static/css/main.00c171b6.chunk.css"
+  },
+  {
+    "revision": "188bf0cfafc49a3b4882",
+    "url": "/Amazon_clone_reactJS/static/js/2.64c1c029.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Amazon_clone_reactJS/static/js/2.64c1c029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c91811896b74c9489a2",
+    "url": "/Amazon_clone_reactJS/static/js/main.bc4a0625.chunk.js"
+  },
+  {
+    "revision": "b1fb0c64ec67bd5e101e",
+    "url": "/Amazon_clone_reactJS/static/js/runtime-main.0d1dd769.js"
+  }
+]);
